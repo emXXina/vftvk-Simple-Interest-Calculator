a5c530387dddca574455c3c471502c002993218a
emxxina <emxxina@gmail.com>
2021-05-23 23:40:32 +0200

    keeps input form from moving around

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function compute() {
     var futureYear = new Date().getFullYear() + parseInt(years);
 
     document.getElementById("result").innerHTML =
-        "<br>If you deposit <mark>" + principal + "</mark>,<br/>"
+        "If you deposit <mark>" + principal + "</mark>,<br/>"
         + "at an interest rate of <mark>" + rate + "%</mark>.<br/>"
         + "You will receive an amout of <mark>" + interest + "</mark>,<br/>"
         + "in the year <mark>" + futureYear + "</mark>.<br>";
