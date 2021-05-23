dbe84c5eb91ec181fd1f14b3b47e58bf5f8860a5
emXXina <emxxina@gmail.com>
2021-05-23 20:58:56 +0000

    display calculated interest

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ function compute() {
     var interest = principal * years * rate /100;
 
     var futureYear = new Date().getFullYear() + parseInt(years);
+
+    document.getElementById("result").innerText = "If you deposit " + principal
+        + ", \nat an interest rate of " + rate + "%.\nYou will receive an amout of "
+        + interest + ",\nin the year 2025."
 }
         
 function updateRate() {
